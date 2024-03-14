ba571125ce1b467e2866ac3927d6eabffd359f65
KimJinMyung <163387551+KimJinMyung@users.noreply.github.com>
2024-03-14 16:29:04 +0900

    2024_03_14
    3월 14일 트리 구조와 이진 트리에 대하여 배웠다.

---
+++ 2024_03_14/CBST.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_03_14
+{
+    internal class CBST
+    {
+        /*
+         * 이진 탐색 트리 (BinarySearchTree)
+         * 이진 속성, 탐색 속성을 적용한 트리
+         * 이진 탐색을 통한 탐색영역을 절반으로 줄여가며 탐색가능.
+         * 균형을 잡은 트리에서 유용.
+         * 
+         * 이진 : 부모 노드는 최대 2개의 자식 노들을 가질 수 있다.
+         * 탐색 : 자신의 노드보다 작은 값은 왼쪽, 큰값들은 오른쪽에 위치한다.
+         * 
+         * [탐색]
+        // 17을 탐색?
+        //                       23 (↙)
+        //         ┌──────┴──────┐
+        //         11 (↘)                     38
+        //   ┌──┴──┐              ┌──┴──┐
+        //   3           19 (↙)         31          65
+        //   └─┐ ┌─┴─┐       ┌─┘          └─┐
+        //        6 [17]    22       24                  87
+         * 루트 노드부터 시작하여 탐색하는 값과 비교.
+         * 작은 경우 왼쪽 자식 노드로...
+         * 큰 경우 오른쪽 자식 노드로 감.
+         * 
+         * 1. 23과 17을 비교 => (17 < 23) -> 왼쪽으로 감.
+         *    ㄴ이제 오른쪽 서브 트리는 탐색할 필요가 없음.
+         * 2. 11과 17을 비교 => (17 > 11) -> 오른쪽으로 감.
+         * 3. 19와 17을 비교 => (17 < 19) -> 왼쪽으로 감.
+         * 4. 17과 17을 비교 => (17 == 17) -> 탐색 완료.         
+         * 
+         * [삽입]
+        // 35를 삽입
+        // 탐색이랑 비슷함.
+        // 루트 노드부터 시작해서 삽입하는 값과 비교.
+        // 작은 경우는 왼쪽 자식 노드로..
+        // 큰 경우는 오른쪽 자식 노드로 이동.
+
+        // [삽입 전]
+        //                       23 (↘)
+        //         ┌──────┴──────┐
+        //         11                          38 (↙)
+        //   ┌──┴──┐              ┌──┴──┐
+        //   3           19              31(↘)      65
+        //   └─┐ ┌─┴─┐       ┌─┘          └─┐
+        //        6 17      22       24                  87
+
+        // [삽입 후]
+        //                       23 
+        //         ┌──────┴──────┐
+        //         11                          38 
+        //   ┌──┴──┐              ┌──┴──┐
+        //   3           19              31          65
+        //   └─┐ ┌─┴─┐       ┌─┴─┐      └─┐
+        //        6 17      22       24      35          87
+         * 
+         * 1. 23과 35를 비교 => (35 > 25) -> 오른쪽으로 감
+         * 2. 38과 35를 비교 => (35 < 38) -> 왼쪽으로 감.
+         * 3. 31과 35를 비교 => (35 > 31) -> 오른쪽으로 감.
+         * 4. 비어있는 공간이면 삽입.
+         * 
+         * [삭제]
+         * 1. 자식이 0개인 경우 어떻게 지울것인가..
+         *  ㄴ 22를 삭제한다고 하면 부모 노드의 자식 노드를 null로 변경
+         * 
+        // [삭제 전]
+        //                       23 
+        //         ┌──────┴──────┐
+        //         11                          38 
+        //   ┌──┴──┐              ┌──┴──┐
+        //   3           19              31          65
+        //   └─┐  ┌─┴─┐      ┌─┴─┐          
+        //        6  17      22      24      35           
+        //
+        // [삭제 후]
+        //                       23 
+        //         ┌──────┴──────┐
+        //         11                          38 
+        //   ┌──┴──┐              ┌──┴──┐
+        //   3           19              31          65
+        //   └─┐  ┌─┘          ┌─┴─┐          
+        //        6  17              24      35         
+
+         * 2. 자식이 1개인 노드의 삭제 : 삭제하는 노드의 부모와 자식을 연결 후 삭제
+         * 38을 삭제 (23과 31을 연결.)
+         *
+        // [삭제 전]
+        //                       23 
+        //         ┌──────┴──────┐
+        //         11                          38 
+        //   ┌──┴──┐              ┌──┴──┐
+        //   3           19              31          65
+        //   └─┐  ┌─┴─┐       ┌─┴─┐          
+        //        6  17      22       24      35        
+        // 
+        // [삭제 후]
+        //                       23 
+        //         ┌──────┴──────┐
+        //         11                          31 
+        //   ┌──┴──┐              ┌──┴──┐
+        //   3           19              24          35          
+        //   └─┐ ┌─┴─┐                
+        //        6 17      22             
+        
+         * 2. 자식이 2개인 노드의 삭제 : 삭제하는 노드를 기준으로 오른쪽 자식중 가장 작은 값 노드와 교체 후 삭제
+         * 23을 삭제
+         * 
+        // 1. 오른쪽 자식중 가장 작은 값 노드를 탐색
+        //                       23 
+        //         ┌──────┴──────┐
+        //         11                          38 
+        //   ┌──┴──┐              ┌──┴──┐
+        //   3           19              24          65
+        //   └─┐  ┌─┴─┐      ┌─┘          
+        //        6  17      22      31       
+
+        // 2. 가장 작은 값 노드와 교체
+        //                       24
+        //         ┌──────┴──────┐
+        //         11                          38 
+        //   ┌──┴──┐              ┌──┴──┐
+        //   3           19              23          65
+        //   └─┐  ┌─┴─┐      ┌─┘          
+        //        6  17      22      31       
+
+        // 3. 삭제
+        //                       24
+        //         ┌──────┴──────┐
+        //         11                          38 
+        //   ┌──┴──┐              ┌──┴──┐
+        //   3           19              31          65
+        //   └─┐  ┌─┴─┐               
+        //        6  17      22             
+
+        // 불균형 구조일때 어떻게 해결되는 것인지 조사할 것.
+         */
+
+        public class BinarySearchTree<T> where T : IComparable<T>
+        {
+            //노드를 나타내는 클래스
+            private class Node
+            {
+                public T item;
+                public Node parent;
+                public Node left;
+                public Node right;
+
+                public Node(T item, Node parent, Node Left, Node right) 
+                { 
+                    this.item = item;
+                    this.parent = parent;
+                    this.left = Left;
+                    this.right = right;
+                }                                
+            }
+
+            private Node root;
+
+            public BinarySearchTree()
+            {
+                this.root = null;
+            }
+
+            //삽입
+            public bool Add(T item)
+            {
+                // 루트 노드가 없으면 새로이 생성해서 집어넣어라.
+                if (root == null)
+                {
+                    Node newNode = new Node(item, null, null, null);
+                    root = newNode;
+                    return true;
+                }
+
+                Node current = root;
+
+                while (current != null)
+                {
+                    // item이 current의 item보다 작으면 음수, 같으면 0, 크면 양수 반환
+                    if (item.CompareTo(current.item) < 0)
+                    {
+                        //왼쪽 노드의 값이 없으면
+                        if (current.left == null)
+                        {
+                            Node newNode = new Node(item, null, null, null);
+                            current.left = newNode;
+                            newNode.parent = current;
+                            break;
+                        }
+                        current = current.left;
+                    }
+                    else if (item.CompareTo(current.item)>0)
+                    {
+                        if(current.right == null)
+                        {
+                            Node newNode = new Node(item, null, null, null);
+                            current.right = newNode;
+                            newNode.parent = current;
+                            break;
+                        }
+                        current = current.right;
+                    }
+                    // 중복 방지
+                    else // if (item.CompareTo(current.item)==0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            } // end of Add
+
+            public bool Remove(T item) 
+            {
+                //아이템이 트리에 있으면 해당 노드를 삭제
+                //없으면 false 반환
+
+                Node  findNode = FindNode(item);
+
+                if (findNode != null)
+                {
+                    EraseNode(findNode);
+                    return true;
+                }
+                else
+                {
+                    return false ;
+                }
+            }
+
+            public bool Contatins(T item)
+            {
+                //찾아서 있으면 true, 없으면 false
+
+                Node node = FindNode(item);
+                if (node != null)
+                {
+                    return true;
+                }else return false ;
+            }
+
+            public void Clear()
+            {
+                root = null;
+            }
+
+            //찾기
+            public Node FindNode(T item)
+            {
+                if (root == null) return null;
+                Node current = root;
+                while (current != null)
+                {
+                    if ( item.CompareTo(current.item) < 0)
+                    {
+                        current = current.left;
+                    }
+                    else if (item.CompareTo(current.item) > 0)
+                    {
+                        current = current.right;
+                    }
+                    else 
+                    { 
+                        return current; 
+                    }
+                }
+                return null;
+            }
+
+            public void EraseNode(Node node)
+            {
+                //입력한 아이템이 노드에 있으면 해당 노드를 제거.
+
+                //자식이 없으면
+                if (node.left == null && node.right == null)
+                {
+                    Node parent = node.parent;
+
+                    if (parent == null) root = null;
+                    else if (parent.left == null) parent.left = null;
+                    else if (parent.right == null) parent.right = null;
+                }
+                //자식을 한개만 가지는 경우
+                else if (node.left != null || node.right != null)
+                {
+                    Node parent = node.parent;
+                    Node child = node.left != null ? node.left : node.right;
+
+                    if (parent == null)
+                    {
+                        root = child;
+                        child.parent = null;
+                    }else if (parent.left == node)
+                    {
+                        parent.left = child;
+                        child.parent = parent;
+                    }else if (parent.right == node)
+                    {
+                        parent.right = child;
+                        child.parent = parent;
+                    }
+                }
+                //자식을 두개 모두 가지고 있으면.
+                else
+                {
+                    Node nextNode = node.right;
+                    while (nextNode.left != null)
+                    {
+                        nextNode = nextNode.left;
+                    }
+                    node.item = nextNode.item;
+                    EraseNode(nextNode);
+                }
+            }
+        }
+
+        static void Main()
+        {
+            //사용하면 전위 순회로 실행되어 오름차순으로 정렬된다.
+            BinarySearchTree<int> bst = new BinarySearchTree<int>();
+
+            bst.Add(50);
+            bst.Add(30);
+            bst.Add(70);
+            bst.Add(20);
+            bst.Add(40);
+            bst.Add(60);
+            bst.Add(80);
+
+            Console.WriteLine(bst.Contatins(40));
+        }
+    }
+}
---
+++ 2024_03_14/Program.cs
@@ -0,0 +1,137 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _2024_03_14
+{
+    /*
+     * 트리 (Tree)
+     * 계층적인 자료를 나타내는 자주 사용하는 자료구조
+     * 
+     * 부모노드가 여러 자식노드들을 가질 수 있는 1 : N 구조
+     * 
+     * 비선형적인 구조
+     * 대칭적인 구조를 나타내고 있다.
+     * 
+     * [구성 요소]
+     * 부모   : 루트 노드 방향으로 직접 연결된 노드
+     * 자식   : 루트 노드 반대 방향으로 직접 연결된 노드
+     * 뿌리(root)     : 부모 노드가 없는 최상위 노드, 트리의 깊이 0에 하나만 존재
+     * 가지(branch)   : 부모 노드와 자식노드가 모두 있는 노드, 트리의 중간에 존재
+     * 잎     : 자식 노드가 없는 노드, 트리의 끝에 존재
+     * 길이   : 출발 노드에서 도착노드까지 거치는 수
+     * 깊이   : 루트 노드부터의 길이, 0부터 시작.
+     * 차수   : 자식 노드의 갯수
+     * 
+     * 계층 구조를 가질 수 있는 대칭적인 자료 구조,
+     * 효율적인 검색에 용이
+     * 
+     * 자식 노드의 갯수가 정해져있으면 배열로,
+     * 정해지지 않았으면 리스트로 구현한다.
+     * 
+     * [이진 트리] binary tree
+     * 부모 노드가 자식 노드를 최대 2개까지만 가질 수 있는 트리.
+     * 일반적으로 이진트리로 구현한다.
+     * 
+     * [이진 트리 순회 방식]
+     * 비선형적인 자료구조이기 때문에 순서에 대하여 규칙이 있어야한다.
+     * 전위 : 노드 -> 왼쪽 -> 오른쪽
+     * 중위 : 왼쪽 -> 노드 -> 오른쪽
+     * 후위 : 왼쪽 -> 오른쪽 -> 노드
+     *
+     *===============================================================
+     */
+
+    //Recursive Function : 재귀 함수
+    //자기 자신을 호출하는 것
+    //반드시 종료 조건이 호출해야 한다.
+    //없으면 무한 루프가 발생하여 스택 오버 플로우가 발생한다.
+    //단점 : 반복문보다 속도가 떨어질 수 있음.
+
+    //public void Print()
+    //{
+    //    Print();
+    //}
+
+    //구현 방식  
+    //0. 이진 트리의 노드 클래스 제작
+    public class BinaryNode<T>
+    {                            
+        //1. 데이터를 담을 배열 및 리스트 선언
+        public T item;
+
+        //2. 부모 노드 선언
+        public BinaryNode<T> parent;
+
+        //3. 자식 노드 2개를 선언.
+        public BinaryNode<T> left;
+        public BinaryNode<T> right;
+
+        public BinaryNode(T item)
+        {
+            this.item = item;            
+        }
+    }
+
+    //0-1. 이진 트리 클래스 제작
+    public class BinaryTree<T>
+    {
+        //4. 루트 노드 선언
+        private BinaryNode<T> root;
+
+        public BinaryTree(BinaryNode<T> root)
+        {
+            this.root = root;
+        }
+
+        //전위 순회
+        public void PreOrder(BinaryNode<T> node)
+        {
+            if (root != null)
+            {
+                Console.Write(node.item + " -> ");
+                PreOrder(node.left);
+                PreOrder(node.right);
+            }
+        }
+
+        //중위 순회
+        public void InOrder(BinaryNode<T> node)
+        {
+            if(root != null)
+            {
+                PreOrder(root.left);
+                Console.WriteLine(node.item + " -> ");
+                PreOrder(root.right);
+            }
+        }
+
+        //하위 순회
+        public void PostOrder(BinaryNode<T> node)
+        {
+            if (root != null)
+            {
+                PreOrder(root.left);
+                PreOrder(root.right);
+                Console.WriteLine(node.item + " -> ");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryNode<char> root = new BinaryNode<char>('A');
+            root.left = new BinaryNode<char>('B');
+            root.right = new BinaryNode<char>('C');
+            root.left.left = new BinaryNode<char>('D');
+            root.left.right = new BinaryNode<char>('E');
+            root.right.left = new BinaryNode<char>('F');
+            root.right.right = new BinaryNode<char>('G');
+
+            BinaryTree<char> tree = new BinaryTree<char>(root);
+
+            Console.WriteLine("전위 순회");
+            tree.PreOrder(root);
+        }
+    }
+}
